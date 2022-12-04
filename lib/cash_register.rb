117fcb22feffa84d73b4e13149f3a9f419e1b7e9
Elaine <elainedoud@gmail.com>
2022-12-04 16:21:55 -0500

    commit tree 994a06719073218532fcb3fefe21a44124c121d2

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,19 @@
+class CashRegister
 
+def self.new(employee_discount)
+    @employee_discount = employee_discount
+    @total = 0
+end
+
+def total
+    @total = total
+end
+
+def add_item(quantity)
+    @title = title
+    @price = price
+    @total = total + price
+end
+
+
+end
